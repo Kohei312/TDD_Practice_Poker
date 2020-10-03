91c1598d59e00a805aa9ff3de2362f58a0c0f48d
KoheiYoshida-3 <yes.kohei312@gmail.com>
2020-10-03 15:35:23 +0900

    Cardの定義を確認 1. Cardがもつ不変の性質(Suit,Rank)をenumで定義 2. Cardの状態を持つ変数 notify を定義

--- TDD_Practice_Poker/TDD_Practice_PokerTests/TDD_Practice_PokerTests.swift
+++ TDD_Practice_Poker/TDD_Practice_PokerTests/TDD_Practice_PokerTests.swift
@@ -8,26 +8,37 @@
 import XCTest
 @testable import TDD_Practice_Poker
 
+
+// TODO:- Cardを定義する
+// 1. Rankをenumで作成(不変の定義のため)
+// 2. Suitをenumで作成(不変の定義のため)
 class TDD_Practice_PokerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+//  MARK:- 動作確認済み
+//    func testInitializeCard(){
+//        var card:Card
+//
+//        card = Card(suit: .club, rank: .ace)
+//        XCTAssertEqual(card.suit, .heart)
+//        XCTAssertEqual(card.rank, .three)
+        
+//        card = Card(suit: .spade, rank: .jack)
+//        XCTAssertEqual(card.suit, .heart)
+//        XCTAssertEqual(card.rank, .three)
+        
+//    }
+  
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+//  MARK:- 動作確認済み
+//    func testNotifyCard(){
+//
+//        var card:Card
+//
+//        card = Card(suit: .diamond, rank: .eight)
+//        XCTAssertEqual(card.notify, "♦︎8")
+//
+//        card = Card(suit: .spade, rank: .three)
+//        XCTAssertEqual(card.notify, "♠︎3")
+//    }
+    
 }
